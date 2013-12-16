f0a327c16589ba63acbb9c989849935fee53d58f
kkurni <kkurni@gmail.com>
2013-12-16 16:47:11 +1100

    add samples with ravendb

---
+++ Ordering.Server/PlaceOrderHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using Ordering.Messages;
+
+namespace Ordering.Server
+{
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        public IBus Bus { get; set; }
+
+        public void Handle(PlaceOrder message)
+        {
+            Console.WriteLine(@"Order for Product:{0} placed", message.Product);
+
+             //throw new Exception("Uh oh - something went wrong....");
+
+            Console.WriteLine(@"Publishing: OrderPlaceed for Order Id: {0}", message.Id);
+
+            Bus.Publish<OrderPlaceed>(e => { e.OrderId = message.Id; });
+        }
+    }
+}
---
+++ Ordering.Subscriber2/OrderPlaceedHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using Ordering.Messages;
+
+namespace Ordering.Subscriber2
+{
+    public class OrderPlaceedHandler : IHandleMessages<OrderPlaceed>
+    {
+        public IBus Bus { get; set; }
+
+        public void Handle(OrderPlaceed message)
+        {
+            Console.WriteLine(@"Handling: OrderPlaced for Order Id: {0}", message.OrderId);
+        }
+    }
+}
